5bb540cede6e0fd993051599486408626cc5bac3
invalybeshari <128167600+invalybeshari@users.noreply.github.com>
2024-01-09 23:39:05 +0100

    Update StudyProgram.sql

--- StudyProgram.sql
+++ StudyProgram.sql
@@ -1,9 +1,9 @@
 CREATE TABLE StudyProgram (
-    StudyProgramID INT PRIMARY KEY,
+    StudyProgramID INT,
     StudyProgramName VARCHAR(255),
     LecturersID INT,
     CourseSemesterID INT,
+  	PRIMARY KEY (studyprogramid, studyprogramname),
     FOREIGN KEY (LecturersID) REFERENCES Lecturers(LecturersID),
     FOREIGN KEY (CourseSemesterID) REFERENCES Courses(CourseSemId) 
 );
-
