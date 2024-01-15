461c9caad23d41323ea67ea6699f3191752ec216
mbaci22 <128814801+mbaci22@users.noreply.github.com>
2024-01-15 22:36:41 +0100

    Delete StudentsOverGPA.sql

--- StudentsOverGPA.sql
+++
@@ -1,6 +0,0 @@
---Find students who have a GPA above the departmental average:
-
-SELECT StudentID, StudentName, GPA
-FROM Transcript
-WHERE GPA > (SELECT AVG(GPA) 
-FROM Transcript WHERE FacultyID = DepartmentID);
