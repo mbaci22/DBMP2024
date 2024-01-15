3f20c315d7e233294839b8ca6d281cbdf153b87e
invalybeshari <128167600+invalybeshari@users.noreply.github.com>
2024-01-15 17:28:59 +0100

    Update Transcript.sql

--- Transcript.sql
+++ Transcript.sql
@@ -1,10 +1,8 @@
 CREATE TABLE Transcript (
     StudentID INT,
     GPA DECIMAL(3, 2), 
-    FacultyID INT,
     StudyProgramID INT,
     FOREIGN KEY (StudentID) REFERENCES Students(StudentID), 
-    FOREIGN KEY (FacultyID) REFERENCES Faculty(FacultyID), 
     FOREIGN KEY (StudyProgramID) REFERENCES StudyProgram(StudyProgramID)
 );
 
