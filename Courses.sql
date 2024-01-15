88e363e9d0e32e8a397a862b4eeabdde051e3003
invalybeshari <128167600+invalybeshari@users.noreply.github.com>
2024-01-15 17:32:17 +0100

    Update Courses.sql

--- Courses.sql
+++ Courses.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Courses (
     CourseID INT,
+    CourseSemId INT,
     CourseName VARCHAR(255),
     CourseType VARCHAR(20) CHECK (CourseType IN ('mandatory', 'elective')),
     Credits INT,
     TotalHours INT,
     Attendance INT,
-    CourseSemId INT,
     PRIMARY KEY (CourseID, CourseSemId)
+    FOREIGN KEY (LecturersID) REFERENCES Lecturers (LecturersID)
 );
