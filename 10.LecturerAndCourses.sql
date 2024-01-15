2e584d2f1adc2fb2c1b2674da09bf8f01186d63b
mbaci22 <128814801+mbaci22@users.noreply.github.com>
2024-01-15 22:30:56 +0100

    Update and rename LecturerAndCourses.sql to 10.LecturerAndCourses.sql

--- 10.LecturerAndCourses.sql
+++ 10.LecturerAndCourses.sql
@@ -1,8 +1,5 @@
 --List all lecturers and the courses they teach:
 
 SELECT Lecturers.LecturersName, Lecturers.LecturersSurname, Courses.CourseName
-FROM Lecturers
+FROM Lecturers, Courses
 JOIN Faculty ON Lecturers.LecturersID = Faculty.LecturersID
-JOIN Courses ON Faculty.FacultyID = Courses.LecturersID;
-
-
