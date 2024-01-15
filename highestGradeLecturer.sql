6e1e0afd4af162f7888ec881794709fffe1acfd2
Saadet28 <128159234+Saadet28@users.noreply.github.com>
2024-01-15 23:32:34 +0100

    Update highestGradeLecturer.sql

--- highestGradeLecturer.sql
+++ highestGradeLecturer.sql
@@ -1,1 +1,22 @@
-Retrieve Courses with the Highest Average Final Grade
+--Retrieve the names of lecturers who teach courses with the highest final grade
+SELECT
+    L.LecturersID,
+    L.LecturersName,
+    L.LecturersSurname,
+    C.CourseID,
+    C.CourseSemID,
+    C.CourseName,
+    G.FinalGrade
+FROM
+    Lecturers L
+JOIN
+    Courses C ON L.LecturersID = C.LecturersID
+JOIN
+    Grade G ON C.CourseID = G.CourseID AND C.CourseSemID = G.CourseSemID
+WHERE
+    G.FinalGrade = (
+        SELECT MIN(FinalGrade)
+        FROM Grade
+    );
+
+
