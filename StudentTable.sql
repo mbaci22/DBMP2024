de25c48bf55e27fbf9180a327f1179dd2d6a31f3
Saadet28 <128159234+Saadet28@users.noreply.github.com>
2024-01-15 20:36:05 +0100

    Update StudentTable.sql -- added studyprogramid

--- StudentTable.sql
+++ StudentTable.sql
@@ -1,14 +1,15 @@
 CREATE TABLE Student (
+  StudyProgramID INT NOT NULL,
   StudentID INT NOT NULL,
   StudentName VARCHAR (30) NOT NULL,
   StudentSurname VARCHAR (30) NOT NULL,
   StudentBirthday DATE NOT NULL,
   StudentBirthPlace VARCHAR (30),
   StudentSex CHAR (1),
+  StudentEmail VARCHAR (50) NOT NULL, 
   StudentMaritalStatus VARCHAR (35),
   StudentCitizenship VARCHAR (30) NOT NULL,
-  StudentEmail VARCHAR (50) NOT NULL, --epoka mail already has a standard form of mail which is the initial of the name and the whole surname
-  StudyProgramID INT NOT NULL,
+  NationalID VARCHAR(10);
   PRIMARY KEY (StudentID),
   FOREIGN KEY (StudyProgramID) REFERENCES StudyProgram(StudyProgramID) 
 );
