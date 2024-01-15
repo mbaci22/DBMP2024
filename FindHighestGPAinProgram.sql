5acf3f6d71b2223b1453c907086eccf18ca56bc8
Saadet28 <128159234+Saadet28@users.noreply.github.com>
2024-01-15 23:18:25 +0100

    Create FindHighestGPAinProgram.sql

---
+++ FindHighestGPAinProgram.sql
@@ -0,0 +1,14 @@
+--Retrieve Students with the Highest GPA in a Specific Study Program:
+SELECT
+    S.StudentID,
+    S.StudentName,
+    S.StudentSurname,
+    T.GPA
+FROM
+    Student S
+JOIN
+    Transcript T ON S.StudentID = T.StudentID
+WHERE
+    S.StudyProgramID = 001
+ORDER BY
+    T.GPA DESC; 
