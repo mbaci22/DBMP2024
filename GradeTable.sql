85f69eb8d5417470720358e51a92ceae640cf86f
Saadet28 <128159234+Saadet28@users.noreply.github.com>
2024-01-15 19:30:41 +0100

    Update GradeTable.sql

--- GradeTable.sql
+++ GradeTable.sql
@@ -1,10 +1,19 @@
 CREATE TABLE Grade (
-  	  GradeName VARCHAR (20) NOT NULL,
-  	  GradeType VARCHAR (20) NOT NULL,
-  	  GradeWeight INT (100) NOT NULL, --as it is out of a hundred
   	  FinalGrade CHAR (2) NOT NULL,
+  	  FinalExam FLOAT,
+  	  MidtermExam FLOAT,
+      Quizes FLOAT,
+      Assigmements FLOAT,
+  	  Projects FLOAT,
+  	  FinalWeight FLOAT,
+  	  MidtermWeight FLOAT,
+      QuizesWeight FLOAT,
+  	  AssigmentsWeight FLOAT,
+  	  ProjectWeight FLOAT,
   	  CourseID INT,
+  	  CourseSemID INT,
   	  StudentID INT,
+  	  FOREIGN KEY (CourseSemID) REFERENCES Courses (CourseSemID),
       FOREIGN KEY (CourseID) REFERENCES Courses (CourseID),
       FOREIGN KEY (StudentID) REFERENCES Student (StudentID) 
       );
