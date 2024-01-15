bebb90a2a8c2bebce23f2b595b721a45d6d59073
gerisonh <68287989+gerisonh@users.noreply.github.com>
2024-01-15 23:38:07 +0100

    Create HigestFinalGrade.sql

---
+++ HigestFinalGrade.sql
@@ -0,0 +1,21 @@
+--Retrieve the names of lecturers who teach courses with the highest final grade
+SELECT
+    L.LecturersID,
+    L.LecturersName,
+    L.LecturersSurname,
+    C.CourseID,
+    C.CourseSemID,
+    C.CourseName,
+    G.FinalGrade
+FROM
+    Lecturers L
+JOIN
+    Courses C ON L.LecturersID = C.LecturersID
+JOIN
+    Grade G ON C.CourseID = G.CourseID AND C.CourseSemID = G.CourseSemID
+WHERE
+    G.FinalGrade = (
+        SELECT MIN(FinalGrade)
+        FROM Grade
+    );
+
